76c12165e9014bcdc8c215da87356d6508c6cae4
feixiaobo <feixiaobo@wecash.net>
2017-01-08 23:25:24 +0800

    redis 发布订阅机制

--- src/main/java/exercise/redisPubSub/PubSubscriber.java
+++ src/main/java/exercise/redisPubSub/PubSubscriber.java
@@ -1,30 +1,39 @@
 package exercise.redisPubSub;
 
+import exercise.utils.Constants;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPubSub;
 
+import javax.annotation.PostConstruct;
+
+@Component
 public class PubSubScriber extends JedisPubSub {
 
+    private Logger logger = LogManager.getLogger(PubSubScriber.class);
+
     @Autowired
+    private JedisPool jedisPool;
     private Jedis jedis;
 
     public PubSubScriber() {
     }
 
     public void onMessage(String channel, String message) {
-        System.out.println(String.format("receive redis published message, channel :-> %s, message :-> %s", channel, message));
+        logger.info("receive redis published message, channel :-> {}, message :-> {}", channel, message);
         this.handle(channel, message);
     }
 
     public void onSubscribe(String channel, int subscribedChannels) {
-        System.out.println(String.format("subscribe redis channel success, channel :> %s, subscribedChannels :> %d",
-                channel, subscribedChannels));
+        logger.info("subscribe redis channel success, channel :> {}, subscribedChannels :> {}", channel, subscribedChannels);
     }
 
     public void onUnsubscribe(String channel, int subscribedChannels) {
-        System.out.println(String.format("unsubscribe redis channel, channel :- %s, subscribedChannels :- %d",
-                channel, subscribedChannels));
+        logger.info("unsubscribe redis channel, channel :- {}, subscribedChannels :- {}", channel, subscribedChannels);
         this.unsubscribe(channel);
     }
 
@@ -34,45 +43,24 @@ public class PubSubScriber extends JedisPubSub {
         if(index < 0){
             return;
         }
-        Long txid = Long.valueOf(message.substring(0,index));
-        String key = channel;
-        while (true){
-            String lm = jedis.lindex(key,0);  //以同一个key存储的list的第一个value
-            if(lm == null){
-                break;
-            }
-            int li = lm.indexOf("/");
-            //如果消息不合法，删除并处理
-            if(li < 0){
-                String result = jedis.lpop(key);//删除当前message
-                //为空
-                if(result == null){
-                    break;
-                }
-                //message(channel, lm);
-                //                process.subProcess(channel,message);
-                this.process(channel, message);
-                continue;
-            }
-            Long lxid = Long.valueOf(lm.substring(0,li));//获取消息的txid
-            //直接消费txid之前的残留消息
-            if(txid >= lxid){
-                jedis.lpop(key);//删除当前message
-                //message(channel, lm);
-                continue;
-            }else{
-                break;
-            }
-        }
+        jedis.lpop(message);//删除当前message
+        message = message.substring(index+1);
+        this.process(channel, message);
     }
 
     public void unsubscribe(String channel){
         String key = channel;
-        jedis.srem("test", key);//从“活跃订阅者”集合中删除
+        jedis.srem(Constants.SUBSCRIBE_CENTER, key);//从“活跃订阅者”集合中删除
         jedis.del(key);//删除“订阅者消息队列”
     }
 
     public void process(String channel, String message){
         //do nothing
+        logger.info("test redis pubsub-----------{}",channel+message);
+    }
+
+    @PostConstruct
+    private  void initJedis(){
+        jedis = jedisPool.getResource();
     }
 }
